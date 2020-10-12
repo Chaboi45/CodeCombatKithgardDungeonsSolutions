71e5f00400e6dd55be869ef6b3f84b68c122ace5
Chaboi3000 <29741391+Chaboi45@users.noreply.github.com>
2020-10-12 15:14:06 +0900

    Create TrueNames.py

---
+++ True Names/TrueNames.py
@@ -0,0 +1,10 @@
+# Defend against "Brak" and "Treg"!
+# You must attack small ogres twice.
+
+hero.moveRight()
+hero.attack("Brak")
+hero.attack("Brak")
+hero.attack("Treg")
+hero.attack("Treg")
+hero.moveRight()
+
