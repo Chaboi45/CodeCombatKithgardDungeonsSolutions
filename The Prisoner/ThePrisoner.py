79a0da05005fee5aa01668e1fbf4f649369cfbed
Chaboi3000 <29741391+Chaboi45@users.noreply.github.com>
2020-10-12 15:16:54 +0900

    Create ThePrisoner.py

---
+++ The Prisoner/ThePrisoner.py
@@ -0,0 +1,13 @@
+# Free the prisoner, defeat the guard and grab the gem.
+hero.moveRight()
+
+# Free Patrick from behind the "Weak Door".
+hero.attack("Weak Door")
+hero.moveRight(2)
+hero.moveDown(3)
+hero.moveRight()
+hero.moveUp(3)
+
+# Defeat the guard, named "Two".
+
+# Get the gem.
